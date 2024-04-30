d6ef5670dabc4564bc2f6e4eeb05e6bdcdadc147
FrancescoGonzalez <117485993+FrancescoGonzalez@users.noreply.github.com>
2024-04-30 20:43:16 +0200

    initial commit

---
+++ src/test/java/org/gonzalez/finalprojectm320/FinalProjectM320ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gonzalez.finalprojectm320;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectM320ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
