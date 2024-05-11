cea2508af8f607c2156d191e1c9a8ad5bb32769d
FrancescoGonzalez <francibgonza21@gmail.com>
2024-05-11 23:51:56 +0200

    commit

--- src/main/java/org/gonzalez/finalprojectm320/config/SpringJdbcConfig.java
+++ src/main/java/org/gonzalez/finalprojectm320/config/SpringJdbcConfig.java
@@ -16,7 +16,7 @@ public class SpringJdbcConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:file:~/db");
+        dataSource.setUrl("jdbc:h2:file:~/databaseM320");
         dataSource.setUsername("sa");
         dataSource.setPassword("");
         return dataSource;
