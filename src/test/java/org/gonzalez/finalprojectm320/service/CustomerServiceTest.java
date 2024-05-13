ba866e29ef84c021ac2a15c7f4aa8ce5a387a69e
FrancescoGonzalez <francibgonza21@gmail.com>
2024-05-13 12:18:25 +0200

    test

--- src/test/java/org/gonzalez/finalprojectm320/service/CustomerServiceTest.java
+++ src/test/java/org/gonzalez/finalprojectm320/service/CustomerServiceTest.java
@@ -1,20 +1,40 @@
 package org.gonzalez.finalprojectm320.service;
 
+import org.gonzalez.finalprojectm320.model.Customer;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@SpringBootTest
 class CustomerServiceTest {
+
+    @Autowired
+    private CustomerService service;
 
     @Test
     void createCustomer() {
+        Customer customer = new Customer(0, "Etienne", "Summermatter", LocalDate.of(2000, 1, 1), "ch");
+        boolean result = service.createCustomer(customer);
+        assertEquals(true, result);
     }
 
     @Test
     void getCustomers() {
+        List<Customer> result = service.getCustomers();
+        assertThat(result.size()).isGreaterThan(0);
     }
 
     @Test
     void getCustomer() {
+        Customer result = service.getCustomer(1);
+        assertEquals(1, result.id());
     }
 }
--- src/test/java/org/gonzalez/finalprojectm320/service/RoomServiceTest.java
+++ src/test/java/org/gonzalez/finalprojectm320/service/RoomServiceTest.java
@@ -1,20 +1,39 @@
 package org.gonzalez.finalprojectm320.service;
 
+import org.gonzalez.finalprojectm320.model.Customer;
+import org.gonzalez.finalprojectm320.model.Room;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class RoomServiceTest {
+
+    @Autowired
+    private RoomService service;
 
     @Test
     void createRoom() {
+        Room r = new Room(0, 1, "SINGLE", 1, 12);
+        boolean result = service.createRoom(r);
+        assertEquals(true, result);
     }
 
     @Test
     void getRoom() {
+        List<Room> result = service.getRooms();
+        assertThat(result.size()).isGreaterThan(0);
     }
 
     @Test
     void getRooms() {
+        Room result = service.getRoom(1);
+        assertEquals(1, result.id());
     }
 }
