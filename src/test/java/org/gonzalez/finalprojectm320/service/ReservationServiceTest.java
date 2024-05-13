cd5de5980c7999cbeec9cb140906deadc0cb6cf2
FrancescoGonzalez <francibgonza21@gmail.com>
2024-05-13 13:24:08 +0200

    test

--- src/test/java/org/gonzalez/finalprojectm320/service/ReservationServiceTest.java
+++ src/test/java/org/gonzalez/finalprojectm320/service/ReservationServiceTest.java
@@ -1,10 +1,8 @@
 package org.gonzalez.finalprojectm320.service;
 
 import org.gonzalez.finalprojectm320.model.Reservation;
-import org.gonzalez.finalprojectm320.repository.interfaces.ReservationRepository;
 import org.gonzalez.finalprojectm320.repository.interfaces.RoomRepository;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,7 +11,6 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class ReservationServiceTest {
@@ -21,17 +18,11 @@ class ReservationServiceTest {
     @Autowired
     private ReservationService service;
 
-    @Autowired
-    private ReservationRepository reservationRepository;
-
-    @Autowired
-    private RoomRepository roomRepository;
-
     @Test
     void createReservation() {
-        Reservation reservation = new Reservation(0, 1, 1, 1, LocalDate.of(2021, 1, 1), LocalDate.of(2021, 1, 2));
+        Reservation reservation = new Reservation(0, 1, 1, 2, LocalDate.of(2021, 1, 10), LocalDate.of(2021, 1, 20));
         boolean result = service.createReservation(reservation);
-        assertEquals(true, result);
+        assertTrue(result);
     }
 
     @Test
